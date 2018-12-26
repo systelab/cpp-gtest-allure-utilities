a591e61f3cac76e000801aefc67ecfa48eeb915c
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2018-12-26 12:44:00 +0100

    Clean up of unnecessary includes

--- test/UnitTest/main.cpp
+++ test/UnitTest/main.cpp
@@ -1,17 +1,8 @@
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows 10 or later.
-#define _WIN32_WINNT 0x0A00	// Change this to the appropriate value to target other versions of Windows.
-#endif
-
-#include <stdio.h>
-#include <tchar.h>
 #include <memory>
 #include <string>
 
-// TODO: reference additional headers your program requires here
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-
-#define BOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE
 
 
 int main(int argc, char* argv[])
