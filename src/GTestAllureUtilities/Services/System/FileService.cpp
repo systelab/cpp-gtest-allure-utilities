d72fe6ffed5a4df62fbeab066e5d4d232b6e9d57
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-01-22 08:42:52 +0100

    Added exception control when unable to write to file.

--- src/GTestAllureUtilities/Services/System/FileService.cpp
+++ src/GTestAllureUtilities/Services/System/FileService.cpp
@@ -11,9 +11,19 @@ namespace systelab { namespace gtest_allure_utilities { namespace service {
 
 	void FileService::saveFile(const std::string& filePath, const std::string& fileContent) const
 	{
-		std::ofstream ofs(filePath);
-		ofs << fileContent;
-		ofs.close();
+		try
+		{
+			std::ofstream outputFileStream;
+			outputFileStream.exceptions(~std::ofstream::goodbit);
+			outputFileStream.open(filePath);
+
+			outputFileStream << fileContent;
+			outputFileStream.close();
+		}
+		catch (std::ofstream::failure& exc)
+		{
+			throw UnableToWriteFileException(filePath, exc.what());
+		}
 	}
 
 }}}
--- src/GTestAllureUtilities/Services/System/IFileService.h
+++ src/GTestAllureUtilities/Services/System/IFileService.h
@@ -11,6 +11,20 @@ namespace systelab { namespace gtest_allure_utilities { namespace service {
 		virtual ~IFileService() = default;
 
 		virtual void saveFile(const std::string& filePath, const std::string& fileContent) const = 0;
+
+	public:
+		struct UnableToWriteFileException : std::runtime_error
+		{
+			UnableToWriteFileException(const std::string& filepath,
+									   const std::string& detailedError)
+				:std::runtime_error("Unable to write file to disk")
+				,m_filepath(filepath)
+				,m_detailedError(detailedError)
+			{}
+
+			std::string m_filepath;
+			std::string m_detailedError;
+		};
 	};
 
 }}}
