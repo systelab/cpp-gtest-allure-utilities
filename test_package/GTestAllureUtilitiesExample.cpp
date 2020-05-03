1afbcecaeba123b40d5bc15a05a4e76c0e62ad5f
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-05-03 19:13:18 +0200

    Fixed compilation of package test

--- test_package/GTestAllureUtilitiesExample.cpp
+++ test_package/GTestAllureUtilitiesExample.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-    auto listener = systelab::gtest_allure::AllureAPI.buildListener();
+    auto listener = systelab::gtest_allure::AllureAPI::buildListener();
 	std::cout << "Listener successfully built!" << std::endl;
 
     return 0;
