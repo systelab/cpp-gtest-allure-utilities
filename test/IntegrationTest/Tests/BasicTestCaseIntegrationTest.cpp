4d130d4d982f96c0151bf0d9b766015622f19872
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-05-10 19:25:27 +0200

    Fixed failing integration tests

--- test/IntegrationTest/Tests/BasicTestCaseIntegrationTest.cpp
+++ test/IntegrationTest/Tests/BasicTestCaseIntegrationTest.cpp
@@ -24,7 +24,7 @@ namespace systelab { namespace gtest_allure { namespace unit_test {
 	TEST_F(BasicTestCaseIntegrationTest, testProgramWithSingleTestSuite)
 	{
 		AllureAPI::setOutputFolder("IntegrationTest\\OutputFolder");
-		AllureAPI::setTestProgramName("SingleBasicTestCase");
+		AllureAPI::setTestProgramName("TestProgramName");
 
 		auto& listener = getEventListener();
 		listener.onProgramStart();
@@ -32,15 +32,21 @@ namespace systelab { namespace gtest_allure { namespace unit_test {
 		setCurrentTime(111);
 		setNextUUIDToGenerate("12345678-1234-1234-1234-123456789012");
 		listener.onTestSuiteStart("SingleTestSuite");
-		listener.onTestStart("SingleTestCase");
+
 		setCurrentTime(222);
+		listener.onTestStart("SingleTestCase");
+
+		setCurrentTime(333);
 		listener.onTestEnd(model::Status::PASSED);
+
+		setCurrentTime(444);
 		listener.onTestSuiteEnd(model::Status::PASSED);
+
 		listener.onProgramEnd();
 
 		ASSERT_EQ(1, getSavedFilesCount());
 		StubFile savedFile = getSavedFile(0);
-		ASSERT_EQ("IntegrationTest\\OutputFolder\\12345678-1234-1234-1234-123456789012-SingleBasicTestCase.json", savedFile.m_path);
+		ASSERT_EQ("IntegrationTest\\OutputFolder\\12345678-1234-1234-1234-123456789012-TestProgramName.json", savedFile.m_path);
 
 		std::string expectedSavedFileContent =
 			"{\n"
@@ -49,7 +55,17 @@ namespace systelab { namespace gtest_allure { namespace unit_test {
 			"    \"status\": \"passed\",\n"
 			"    \"stage\": \"finished\",\n"
 			"    \"start\": 111,\n"
-			"    \"stop\": 222\n"
+			"    \"stop\": 444,\n"
+			"    \"steps\":\n"
+			"    [\n"
+			"        {\n"
+			"            \"name\": \"Action: SingleTestCase\",\n"
+			"            \"status\": \"passed\",\n"
+			"            \"stage\": \"finished\",\n"
+			"            \"start\": 222,\n"
+			"            \"stop\": 333\n"
+			"        }\n"
+			"    ]\n"
 			"}";
 
 		ASSERT_TRUE(compareJSONs(expectedSavedFileContent, savedFile.m_content, getJSONAdapter()));
@@ -58,22 +74,32 @@ namespace systelab { namespace gtest_allure { namespace unit_test {
 	TEST_F(BasicTestCaseIntegrationTest, testProgramWithCoupleOfTestSuites)
 	{
 		AllureAPI::setOutputFolder("IntegrationTest\\OutputFolder");
-		AllureAPI::setTestProgramName("CoupleOfBasicTestCases");
+		AllureAPI::setTestProgramName("CoupleOfBasicTestSuites");
 
 		auto& listener = getEventListener();
 		listener.onProgramStart();
 
 		setCurrentTime(101);
 		setNextUUIDToGenerate("UUID1");
+		listener.onTestSuiteStart("TestSuite1");
 		listener.onTestStart("TestCase1");
 		setCurrentTime(201);
 		listener.onTestEnd(model::Status::FAILED);
+		listener.onTestSuiteEnd(model::Status::FAILED);
 
-		setCurrentTime(202);
-		setNextUUIDToGenerate("UUID2");
-		listener.onTestStart("TestCase2");
 		setCurrentTime(301);
+		setNextUUIDToGenerate("UUID2");
+		listener.onTestSuiteStart("TestSuite2");
+		listener.onTestStart("TestCase2.1");
+		setCurrentTime(401);
 		listener.onTestEnd(model::Status::PASSED);
+		listener.onTestStart("TestCase2.2");
+		setCurrentTime(501);
+		listener.onTestEnd(model::Status::PASSED);
+		listener.onTestStart("TestCase2.3");
+		setCurrentTime(601);
+		listener.onTestEnd(model::Status::SKIPPED);
+		listener.onTestSuiteEnd(model::Status::PASSED);
 
 		listener.onProgramEnd();
 
@@ -81,29 +107,63 @@ namespace systelab { namespace gtest_allure { namespace unit_test {
 
 		// First saved file
 		StubFile firstSavedFile = getSavedFile(0);
-		ASSERT_EQ("IntegrationTest\\OutputFolder\\UUID1-CoupleOfBasicTestCases.json", firstSavedFile.m_path);
+		ASSERT_EQ("IntegrationTest\\OutputFolder\\UUID1-CoupleOfBasicTestSuites.json", firstSavedFile.m_path);
 		std::string expectedFirstSavedFileContent =
 			"{\n"
 			"    \"uuid\": \"UUID1\",\n"
-			"    \"name\": \"TestCase1\",\n"
+			"    \"name\": \"TestSuite1\",\n"
 			"    \"status\": \"failed\",\n"
 			"    \"stage\": \"finished\",\n"
 			"    \"start\": 101,\n"
-			"    \"stop\": 201\n"
+			"    \"stop\": 201,\n"
+			"    \"steps\":\n"
+			"    [\n"
+			"        {\n"
+			"            \"name\": \"Action: TestCase1\",\n"
+			"            \"status\": \"failed\",\n"
+			"            \"stage\": \"finished\",\n"
+			"            \"start\": 101,\n"
+			"            \"stop\": 201\n"
+			"        }\n"
+			"    ]\n"
 			"}";
 		ASSERT_TRUE(compareJSONs(expectedFirstSavedFileContent, firstSavedFile.m_content, getJSONAdapter()));
 
 		// Second saved file
 		StubFile secondSavedFile = getSavedFile(1);
-		ASSERT_EQ("IntegrationTest\\OutputFolder\\UUID2-CoupleOfBasicTestCases.json", secondSavedFile.m_path);
+		ASSERT_EQ("IntegrationTest\\OutputFolder\\UUID2-CoupleOfBasicTestSuites.json", secondSavedFile.m_path);
 		std::string expectedSecondSavedFileContent =
 			"{\n"
 			"    \"uuid\": \"UUID2\",\n"
-			"    \"name\": \"TestCase2\",\n"
+			"    \"name\": \"TestSuite2\",\n"
 			"    \"status\": \"passed\",\n"
 			"    \"stage\": \"finished\",\n"
-			"    \"start\": 202,\n"
-			"    \"stop\": 301\n"
+			"    \"start\": 301,\n"
+			"    \"stop\": 601,\n"
+			"    \"steps\":\n"
+			"    [\n"
+			"        {\n"
+			"            \"name\": \"Action: TestCase2.1\",\n"
+			"            \"status\": \"passed\",\n"
+			"            \"stage\": \"finished\",\n"
+			"            \"start\": 301,\n"
+			"            \"stop\": 401\n"
+			"        },\n"
+			"        {\n"
+			"            \"name\": \"Action: TestCase2.2\",\n"
+			"            \"status\": \"passed\",\n"
+			"            \"stage\": \"finished\",\n"
+			"            \"start\": 401,\n"
+			"            \"stop\": 501\n"
+			"        },\n"
+			"        {\n"
+			"            \"name\": \"Action: TestCase2.3\",\n"
+			"            \"status\": \"skipped\",\n"
+			"            \"stage\": \"finished\",\n"
+			"            \"start\": 501,\n"
+			"            \"stop\": 601\n"
+			"        }\n"
+			"    ]\n"
 			"}";
 		ASSERT_TRUE(compareJSONs(expectedSecondSavedFileContent, secondSavedFile.m_content, getJSONAdapter()));
 	}
