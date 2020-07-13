a967cc4bb0f85c4b2df7f822f7271933dfc0b621
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-07-14 00:15:35 +0200

    Added constants for known property names

---
+++ src/GTestAllureUtilities/Model/TestProperty.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace systelab { namespace gtest_allure { namespace model { namespace test_property {
+
+	static const std::string TMS_ID_PROPERTY = "TMS_ID";
+	static const std::string NAME_PROPERTY = "NAME";
+
+}}}}
