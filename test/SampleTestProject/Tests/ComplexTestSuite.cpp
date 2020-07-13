9c84d02a96c0823b81899a82ec10225506d0fa59
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-07-13 22:20:05 +0200

    Added action and expected result lambda functions

--- test/SampleTestProject/Tests/ComplexTestSuite.cpp
+++ test/SampleTestProject/Tests/ComplexTestSuite.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include "GTestAllureUtilities/AllureAPI.h"
 
 
 using namespace testing;
@@ -11,14 +12,50 @@ namespace systelab { namespace gtest_allure { namespace sample_project {
 
 	TEST_F(ComplexTestSuite, testCase1)
 	{
-		unsigned int result = 100 + 23;
-		ASSERT_EQ(123, result);
+		unsigned int sum;
+		AllureAPI::addAction("Calculate sum 100 plus 23", [&sum]() -> void
+		{
+			sum = 100 + 23;
+		});
+
+		AllureAPI::addExpectedResult("Sum value is 123", [sum]() -> void
+		{
+			ASSERT_EQ(123, sum);
+		});
 	}
 
 	TEST_F(ComplexTestSuite, testCase2)
 	{
-		unsigned int result = 100 - 20;
-		ASSERT_EQ(80, result);
+		unsigned int total;
+		AllureAPI::addAction("Calculate total 25 plus 14", [&total]() -> void
+		{
+			total = 25 + 14;
+		});
+
+		AllureAPI::addAction("Add 10 to total", [&total]() -> void
+		{
+			total += 10;
+		});
+
+		AllureAPI::addAction("Remove 7 from total", [&total]() -> void
+		{
+			total -= 7;
+		});
+
+		AllureAPI::addExpectedResult("Total is 42", [total]() -> void
+		{
+			ASSERT_EQ(42, total);
+		});
+
+		AllureAPI::addAction("Remove 42 from total", [&total]() -> void
+		{
+			total -= 42;
+		});
+
+		AllureAPI::addExpectedResult("Total is 0", [total]() -> void
+		{
+			ASSERT_EQ(0, total);
+		});
 	}
 
 }}}
