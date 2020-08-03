2b0ce8a38ade3c542173b82e5fad9d1013ac9246
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-08-03 20:34:32 +0200

    Prepared travis to deploy packages

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function checkErrors
+{
+	if [ $? != 0 ]
+	then
+		echo 
+		echo "An error occured while deploying artifacts."
+		echo
+		exit 1
+	fi
+}
+
+function parseParameters
+{
+	VERSION="None"
+	while getopts "hv:" OPTION
+	do
+		case $OPTION in
+		h)
+			usage #help: show usage section
+			exit 0
+			;;
+		v)
+			VERSION=$OPTARG
+			;;
+		?)
+			echo "Invalid parameter" 
+			usage
+			exit 1
+			;;
+		esac
+	done
+	
+	echo "********** DEPLOY PARAMETERS **********"
+	echo "VERSION: $VERSION"
+}
+
+parseParameters "${@}"
+
+echo -e "\nUploading RapidJSONAdapter/1.0.8@systelab/stable to systelab-bintray...\n\n"
+conan upload "RapidJSONAdapter/1.0.8@systelab/stable" --all -r systelab-bintray --force
+checkErrors
+
+echo -e "\nUploading GTestAllureUtilities/$VERSION@systelab/stable to systelab-bintray...\n\n"
+conan upload "GTestAllureUtilities/$VERSION@systelab/stable" --all -r systelab-bintray --force
+checkErrors
