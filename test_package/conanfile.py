fd55fb3d860bdb3c99f75d33a434572838da289f
aarnaiz-werfen <aarnaiz@werfen.com>
2024-02-20 16:33:38 +0100

    Adjusted test_package conanfile.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,11 +3,6 @@ from conans import ConanFile, CMake
 class GTestAllureUtilitiesTestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
-    options = {"gtest": ["1.7.0", "1.8.1", "1.10.0"]}
-    default_options = "gtest=1.10.0"
-
-    def configure(self):
-        self.options["GTestAllureUtilities"].gtest = self.options.gtest
 
     def build(self):
         cmake = CMake(self)
@@ -16,8 +11,6 @@ class GTestAllureUtilitiesTestPackageConan(ConanFile):
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
 
     def test(self):
         cmake = CMake(self)
